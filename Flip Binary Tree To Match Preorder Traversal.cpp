cb5a8e93219840a444fb570e2d6d62b505aa8ee6
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-03-29 19:19:15 +0530

    Added solution for the march leetcoding challenge in c++

--- Flip Binary Tree To Match Preorder Traversal.cpp
+++ Flip Binary Tree To Match Preorder Traversal.cpp
@@ -2,12 +2,13 @@
 class Solution {
 public:
     int i = 0;
-    bool dfs(TreeNode* root , vector<int>&voyage, vector<int>&ans)
+    bool dfs(TreeNode* root , vector<int>& voyage, vector<int>& ans)
     {
         if(root == NULL)
         {
             return true;
         }
+        // if root node is not matching then the expected preorder traversal can't be created
         if(root->val != voyage[i])
         {
             return false;
@@ -16,18 +17,18 @@ public:
         {
             i++;
         }
+        // this is the case when flipping is done
         if(root->left != NULL && root->left->val != voyage[i])
         {
             ans.push_back(root->val);
             return dfs(root->right, voyage, ans) && dfs(root->left, voyage, ans);
         }
         else
-        {
+        {// this is the case when no flipping is done and normal dfs is applied
             return dfs(root->left, voyage, ans) && dfs(root->right, voyage, ans);
-
         }
     }
-    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) 
+    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage)
     {
         vector<int>ans;
         if(dfs(root, voyage, ans) == true)
@@ -37,6 +38,6 @@ public:
         return {-1};
     }
 };
-
 // tc o(n)
+
 // sc o(logn) (resursion stac till height of binary tree)
